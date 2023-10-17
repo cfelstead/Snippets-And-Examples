1a0270bafa6e06b5f2e10883b230bd9acd60be65
Chris Felstead <chris.felstead@futuressport.com>
2023-10-17 08:15:44 +0100

    Added CPU Multithreading project.

---
+++ CpuMultithreading/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+TimeSpan delay = TimeSpan.FromSeconds(1);
+IEnumerable<int> taskIds = Enumerable.Range(1, 100);
+
+Stopwatch swFast = new();
+swFast.Start();
+// Run as fast as possible
+await Parallel.ForEachAsync(taskIds, async (taskId, cancellationToken) =>
+{
+    await Console.Out.WriteLineAsync($"FAST Starting task {taskId}.");
+    await Task.Delay(delay);
+    await Console.Out.WriteLineAsync($"FAST Completed task {taskId}.");
+});
+swFast.Stop();
+
+
+
+// Run with limited throughput
+ParallelOptions options = new()
+{
+    MaxDegreeOfParallelism = 3
+};
+Stopwatch swLimited = new();
+swLimited.Start();
+await Parallel.ForEachAsync(taskIds, options, async (taskId, cancellationToken) =>
+{
+    await Console.Out.WriteLineAsync($"LIMITED Starting task {taskId}.");
+    await Task.Delay(delay);
+    await Console.Out.WriteLineAsync($"LIMITED Completed task {taskId}.");
+});
+swLimited.Stop();
+
+
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"Linear time: {(taskIds.Count() * delay)}");
+Console.WriteLine($"FAST time: {swFast.Elapsed}");
+Console.WriteLine($"LIMITED time: {swLimited.Elapsed}");
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ I stuggled to find a simple and complete example of building a Fluent API when I
 This API controls the path the developer takes to make sure all properties are provided. It also has a fork in the command path.
 
 The code is commented to help understand the process.
+
+## CPU Multithreading
+
+Shows how to use Parallel.ForEach to split CPU bound work over multiple processors
